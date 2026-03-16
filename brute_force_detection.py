f268eb4c0f40760216cf559ac699191e5f1ba01c
Rikardo30 <ricardoherrera.3030@gmail.com>
2026-03-16 12:51:38 -0500

    Addition of Telegram API for alerting

--- brute_force_detection.py
+++ brute_force_detection.py
@@ -1,6 +1,6 @@
 # Simple brute-force attack detection script for SSH login attempts
 import re
-
+from notifier import send_telegram_alert
 #Constants
 LIMIT = 5
 LOG_PATH = 'auth.log'
@@ -19,7 +19,8 @@ def detect_brute_force():
                 
     for ip, count in ip_attempts.items():
         if count >= LIMIT:
-            print(f'[!] BRUTE FORCE DETECTED: {ip} — {count} failed attempts')
+#We are calling our send_telegram_alert function from the notifier file and applying the ip and count variables
+            send_telegram_alert(ip, count)
 
 if __name__ == "__main__":
     detect_brute_force()
---
+++ notifier.py
@@ -0,0 +1,20 @@
+#A simple Api alerting script using a telegram bot
+#Make sure to keep your token and chat id separate in an ignored file to protect your data
+#The next goal is to move over to slack instead of telegram in order to have a more profesional environment
+
+#Allows us to pull sensitive data from our .env file
+from dotenv import load_dotenv
+import os
+import requests
+
+load_dotenv()
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_ID")
+#We will call this function in our brute force detection script
+def send_telegram_alert(ip, count):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": f"Brute-force attack detected from {ip} with {count} failed attempts."
+    }
+    requests.post(url, data=payload)
